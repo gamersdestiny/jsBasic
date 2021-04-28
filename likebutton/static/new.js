ecc1ec5bdd10f6da26dedf55dc4bdfb07f460038
Guna <gunagang24@gmail.com>
2021-04-28 18:44:25 +0530

    changed like count to decrement

--- likebutton/static/new.js
+++ likebutton/static/new.js
@@ -1,16 +1,19 @@
 function clicke(){
 var count = 1;
+var count2 = 0;
 	var btn = document.getElementById("btn");
 	var disp = document.getElementById("count");
 	var icon = document.getElementById("icon")
 	icon.innerHTML = `<i class="fas fa-heart"></i>`
 	disp.innerHTML = count;
 	btn.onclick = function () {
-		count++;
+		// count++;
 		disp.innerHTML = count;
-		if (count %2 === 0) {
+		if (count2 %2 === 0) {
+			disp.innerHTML = count2++;
 			icon.innerHTML = `<i class="far fa-heart"></i>`
 		} else {
+			disp.innerHTML = count2--;
 			icon.innerHTML = `<i class="fas fa-heart"></i>`
 		}
 
